ea894cddd49c37ba00c5f01371298ea7765efea2
zsndaydayup <zsnyx0219@163.com>
2024-04-11 15:02:42 +0800

    mainversion1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
-#include"base1.hpp"
-#include"base2.hpp"
+#include <iostream>
+#include "base1.hpp"
+#include "base2.hpp"
 
 int main()
 {
-	
+                int x, y, z;
+	x = sum(38, 72);
+                y = difference(85, 54);
+                z = multi(x, y);
+                std::cout << "z = " << z << std::endl;
 	system("pause");
 	return 0;
 }
